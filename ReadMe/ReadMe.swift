7e67d24b8f9f9ce2e05f9e788fed0341b158423f
DaleAbdullahi <abdull53@msu.edu>
2020-07-06 00:58:03 -0500

    Edited the ReadMe swift file

--- ReadMe/ReadMe.swift
+++ ReadMe/ReadMe.swift
@@ -8,29 +8,26 @@
 
 import Foundation
 /*
-    THIS IS JUST FILE FOR REFRENCE:
+    Dear Felipe and  Caio
+ 
+        Below is a short describtion of the reason why i used what patterns/designs etc that i used.
     
-     THIRD PARTY LIBRARIES:
- 
- 
-
-       I did not use any third part libary . I could have used Alamofire for networking and sdWebImage for image loading.
-       Instead i used URLSession for networking and loaded images using inbuilt swift functionalitis
-       I try to always mininise the use of third party libaries as much as i can since relying on third parties can some time bring problems
+          1: THIRD PARTY LIBRARIES:
+               I did not use any third part libary . I could have used Alamofire for networking and sdWebImage for image loading.
+               Instead i used URLSession for networking and loaded images using inbuilt swift functionalitis
+                I try to always mininise the use of third party libaries as much as i can since relying on third parties can some time bring problems
         
+        2:
+                My API link has no video or the movie link homepage so did not do that . I used every variable found in the API
  
-        My API link has no video or the movie link homepage so did not do that . I used every variable found in the API
+        3:
+                Please run with physical iphone 11 or Iphone 11 simulator . I did layout with iphone 11
  
+        4:
+   
+                I used MVC . This is a simple app so both design work without major problems . Please note that I always prefer using MVVM instead of MVC all the time because of the ViewModel relieving a lot of stress from the ViewControllers and also easy testing of ViewModel functionalities compared to functionailties in the ViewControllers.
+        5:
  
-        Please run with physical iphone 11 or Iphone 11 simulator . I did layout with iphone 11
+               I used extenstions almost everywhere to separate code logic with Marks.
  
- 
-        I used MVC . This is a simple app so both design work without major problems . I always prefer using MVVM instead of MVC all the time because of the ViewModel relieving a lot of stress from the ViewControllers and also easy testing of ViewModel functionalities compared to functionailties in the ViewControllers
- 
-
- 
-        I used extenstions almost everywhere to separate code logic with Marks.
- 
-        
-    
  */
