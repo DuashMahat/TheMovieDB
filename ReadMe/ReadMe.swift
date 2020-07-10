5610a9cef49e0045bd46bd5698e714c3973dba16
Abdullahi D <duashmaht@hotmail.com>
2020-07-10 13:29:50 -0500

    Update ReadMe.swift

--- ReadMe/ReadMe.swift
+++ ReadMe/ReadMe.swift
@@ -11,29 +11,7 @@ import Foundation
  
  
  
-    Dear Felipe and  Caio.
-      
-        Below is a short describtion of the reason why i used what patterns/designs etc that i used.
-          0:
-             Did autolayout fixed 
-             
-          1: THIRD PARTY LIBRARIES:
-               I did not use any third part libary . I could have used Alamofire for networking and sdWebImage for image loading.
-               Instead i used URLSession for networking and loaded images using inbuilt swift functionalitis
-                I try to always mininise the use of third party libaries as much as i can since relying on third parties can some time bring problems
-        
-        2:
-                My API link has no video or the movie link homepage so did not do that . I used every variable found in the API
- 
-        
-                
-        4:
    
-                I used MVC . This is a simple app so both design work without major problems . Please note that I always prefer using MVVM instead of MVC all the time because of the ViewModel relieving a lot of stress from the ViewControllers and also easy testing of ViewModel functionalities compared to functionailties in the ViewControllers.
-        5:
- 
-               I used extenstions almost everywhere to separate code logic with Marks.
- 
        
  
  */
